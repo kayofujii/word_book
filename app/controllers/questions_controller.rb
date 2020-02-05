6b884f7c234587a84cee0f4baf63edac9337606e
藤井佳世 <fujiikayo@MacBook-Pro.local>
2020-02-05 15:27:09 +0900

    arrange question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,15 +13,37 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
 
     if @question.save
-       redirect_to action: "index"
-     else
-       redirect_to action: "new"
+      redirect_to action: "index"
+    else
+      redirect_to action: "new"
     end
   end
 
-  private
-
-  def question_params
-    params.require(:question).permit(:q_body, :a_body)
+  def show
+    @question = Question.find(params[:id])
   end
-end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to :action => "index"
+    else
+      redirect_to :action => "index"
+    end
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+    redirect_to action: :index
+  end
+
+    private
+
+    def question_params
+      params.require(:question).permit(:q_body, :a_body)
+    end
+  end
