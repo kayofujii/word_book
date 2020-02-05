373097223abbedfe3c4288605fde83638ee6fcbe
藤井佳世 <fujiikayo@MacBook-Pro.local>
2020-02-05 11:22:46 +0900

    Finish user model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name,
+  uniqueness: true, #一意性
+  presence: true #必須
+
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+  #emailを不必要に
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
+end
